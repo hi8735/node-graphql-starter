e477e9b003473d65ccb65e08cf72cb1ec2325036
matt-sungwook <hisw0509@gmail.com>
2019-02-18 00:02:27 +0900

    Change `getTypeDefs` to `getTypeDefsAsync`

--- src/app.ts
+++ src/app.ts
@@ -3,26 +3,33 @@ import chalk from "chalk";
 import express = require("express");
 
 import { timestampMessage } from "./util/consoleHelper";
-import { getTypeDefs } from "./graphql/schemaReader";
+import { getTypeDefsAsync } from "./graphql/schemaReader";
+import { DocumentNode } from "graphql";
 
+let typeDefs: DocumentNode[];
+getTypeDefsAsync().then((value) => {
+  typeDefs = value;
+  
+  const apolloConfig: Config = {
+    typeDefs: typeDefs,
+    resolvers: {},
+    context: ({ req }: { req: express.Request }) => {
+      const authoriationToken = req.headers.authoriation || '';
+      return { authorizationToken: authoriationToken };
+    },
+    debug: process.env.NODE_ENV !== 'production',
+    formatError: (error: any) => error,
+  };
 
-const apolloConfig: Config = {
-  typeDefs: getTypeDefs(),
-  resolvers: {},
-  context: ({ req }: { req: express.Request }) => {
-    const authoriationToken = req.headers.authoriation || '';
-    return { authorizationToken: authoriationToken };
-  },
-  debug: process.env.NODE_ENV !== 'production',
-  formatError: (error: any) => error,
-};
+  const app = () => {
+    const apolloServer = new ApolloServer(apolloConfig);
+    apolloServer.listen().then(({ url }) => {
+      console.log(chalk.cyanBright(timestampMessage(`server is running @ ${chalk.underline(url)}`)));
+      console.log(chalk.cyan(timestampMessage(`GraphQL Playground is available @ ${chalk.underline(`${url}graphql`)}`)));
+    });
+  };
 
-const app = () => {
-  const apolloServer = new ApolloServer(apolloConfig);
-  apolloServer.listen().then(({ url }) => {
-    console.log(chalk.cyanBright(timestampMessage(`server is running @ ${chalk.underline(url)}`)));
-    console.log(chalk.cyan(timestampMessage(`GraphQL Playground is available @ ${chalk.underline(`${url}graphql`)}`)));
-  });
-};
-
-app();
+  app();
+}).catch((reason) => {
+  console.log(chalk.red(timestampMessage(reason)));
+});
--- src/graphql/schemaReader.ts
+++ src/graphql/schemaReader.ts
@@ -2,26 +2,29 @@ import { gql } from "apollo-server";
 import { DocumentNode } from "graphql";
 import path from "path";
 import { readFileAsync, readDirAsync } from "../util/fsUtil";
+import chalk from "chalk";
+import { timestampMessage } from "../util/consoleHelper";
 
-const sdlPath = path.join(__dirname, '../graphql/');
+// Path that contains GraphQL schema files
+const schemaPath = path.join(__dirname, '../graphql/schemas');
 
-export const getTypeDefs = () => {
-  let filePaths: string[];
-  readDirAsync(sdlPath).then((value) => { filePaths = value; });
+// Since GraphQL file extensions are varied, you may use the suitable extension for your needs
+const graphqlExtention = '.graphql';
 
-  let typeDefs: DocumentNode[];
-  Promise.all(
-    filePaths.map(
-      async (filePath) => {
-        const fileContent = await readFileAsync(filePath, 'utf-8');
+export const getTypeDefsAsync = async () => {
+  const fileNames = await readDirAsync(schemaPath, graphqlExtention);
+
+  console.log(chalk.greenBright(timestampMessage(`${fileNames.length} schema files found:`)))
+  console.log(fileNames.map(file => file).join('\n'));
+
+  const typeDefs: DocumentNode[] = await Promise.all(
+    fileNames.map(
+      async (fileName) => {
+        const fileContent = await readFileAsync(`${schemaPath}/${fileName}`, 'utf-8');
         const sdl = typeof fileContent === 'string' ? fileContent : fileContent.toString('utf8');
         return gql(sdl);
       }
     )
-  ).then(
-    (value) => {
-      typeDefs = value;
-    }
   );
 
   return typeDefs;
